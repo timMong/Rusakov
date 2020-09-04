3d6261d621d77b5e9ba0671c12134e18e3e38a0e
student <student@C239U05>
2020-09-04 14:05:29 +0300

    qaz

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+struct Person
+{
+  public string firstName;
+  public string lastName;
+  public int age;
+  public Person(string _firstName, string _lastName, int _age)
+  {
+    firstName = _firstName;
+    lastName = _lastName; age = _age;
+  }
+  public override string ToString()
+  {
+    return firstName + " " + lastName + ", age " + age;
+  }
+}
+
+namespace ConsoleApp1
+{
+  
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
+    }
+  }
+}
